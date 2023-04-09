cb88d0b871b6d4e13577691e8c15098518281523
Ronald Rodrigues Pereira <57762848+RonMakaveli@users.noreply.github.com>
2023-04-08 21:43:26 -0300

    First Upload

---
+++ renomear.py
@@ -0,0 +1,11 @@
+import os
+
+caminho_pasta = input("Digite o caminho da pasta: ")
+
+for nome_arquivo in os.listdir(caminho_pasta):
+    caminho_completo = os.path.join(caminho_pasta, nome_arquivo)
+    novo_nome = nome_arquivo[10:]
+    novo_caminho = os.path.join(caminho_pasta, novo_nome)
+    os.rename(caminho_completo, novo_caminho)
+
+print("Arquivos renomeados com sucesso!")
